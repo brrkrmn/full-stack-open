b7d07cba5800eada28f0f5d1d8d5e09d850989ae
Berra Karaman <esma.berra.karaman@hotmail.com>
2023-03-29 08:37:03 +0300

    solve exercises 2.16-2.17

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,6 +3,8 @@ import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 import { useState, useEffect } from "react";
 import personService from "./services/persons";
+import Notification from "./components/Notification/Notification";
+import './index.css';
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -10,6 +12,8 @@ const App = () => {
   const [newPhone, setNewPhone] = useState("");
   const [filter, setFilter] = useState("");
   const [filteredPersons, setFilteredPersons] = useState([]);
+  const [notification, setNotification] = useState(null);
+  const [notificationType, setNotificationType] = useState('');
 
   useEffect(() => {
     personService.getAll().then((initialPersons) => {
@@ -25,9 +29,13 @@ const App = () => {
       personService.create(newPerson).then((returnedPerson) => {
         setPersons([...persons, returnedPerson]);
       });
+      setNotificationType('success')
+      setMessage(`${newName} is added.`)
     } else {
       window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`);
       changeNumber();
+      setNotificationType('success')
+      setMessage(`${newName}'s number is changed.`)
     }
     setNewName("");
     setNewPhone("");
@@ -62,11 +70,25 @@ const App = () => {
   };
 
   const handleDeletePerson = async (id) => {
-   await personService.clear(id);
-   const newPersons = await personService.getAll();
-   window.confirm(`Delete person?`)
-   setPersons(newPersons);
+    try {
+      await personService.clear(id);
+      const newPersons = await personService.getAll()
+      window.confirm(`Delete person?`)
+      setPersons(newPersons);
+      setNotificationType('success')
+      setMessage(`Person deleted.`)
+    } catch {
+      setNotificationType('error')
+      setMessage(`Person already deleted.`)
+    }
   };
+
+  const setMessage = (newMessage) => {
+    setNotification(newMessage)
+    setTimeout(() => {
+      setNotification(null)
+    }, 3000)
+  }
 
   return (
     <div>
@@ -74,6 +96,7 @@ const App = () => {
       <Filter filter={filter} handleNewFilter={handleNewFilter} />
 
       <h3>Add a New Number</h3>
+      <Notification message={notification} type={notificationType} />
       <PersonForm
         handleSubmit={handleSubmit}
         newName={newName}
